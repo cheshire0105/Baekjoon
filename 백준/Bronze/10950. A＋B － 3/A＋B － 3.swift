7603eee935003a597436af61e14626e9409da894
cheshire <106953561+cheshire0105@users.noreply.github.com>
2022-06-14 13:15:25 +0900

    [Bronze V] Title: A+B - 3, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.swift
@@ -0,0 +1,9 @@
+var num : Int = Int(readLine()!)!
+
+for _ in 1...num {
+    var test = readLine()!.split(separator: " ").map{ Int($0)!}
+    for _ in 1...1 {
+        print((test[0])+(test[1]))
+
+    }
+}
